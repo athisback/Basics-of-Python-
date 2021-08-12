7f41745d5648e2fe6a25c83856892005f679eb96
athisback <azizamanov10@gmail.com>
2021-08-12 20:10:36 +0300

    3 задание 2 урока

---
+++ 2.3 REAL.py
@@ -0,0 +1,13 @@
+while True:
+    user_month = input('Введите номер месяца от 1 до 12: ')
+    if 0 <= int(user_month) <= 12 and user_month.isdigit():
+        season_1 = ['зима', 'весна', 'лето', 'осень', 'зима']
+        season_2 = {0: 'зима', 1: 'весна', 2: 'лето', 3: 'осень', 4: 'зима'}
+        print(f'Список сезонов - {season_1[int(user_month) // 3]}\nСловарь сезонов - {season_2[int(user_month) // 3]}')
+        break
+    else:
+        print('Ошибка')
+
+
+
+
