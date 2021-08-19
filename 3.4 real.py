3989747e11ced28c7241bfdd3564a748aefb0b59
athisback <azizamanov10@gmail.com>
2021-08-19 17:57:21 +0300

    4 задание 3 урока

---
+++ 3.4 real.py
@@ -0,0 +1,6 @@
+def my_func(arg_1,arg_2):
+    if arg_1 > 0 and arg_2 < 0:
+        qwerty = arg_1 ** arg_2
+    return qwerty
+
+print(my_func(int(input('Enter the first argument - ')), int(input('Enter the second argument - '))))
